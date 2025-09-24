6c610ed04e809bfbae790dc96bb8731b85de0430
Cole Kendrick <kendrick6@llnl.gov>
2025-09-24 14:49:54 -0700

    Add venv setup for tuolumne

--- conda/llnl_toss/create_env.sh
+++ conda/llnl_toss/create_env.sh
@@ -11,12 +11,44 @@ env_file=env.yml
 anaconda_dir=/collab/usr/gapps/python/${SYS_TYPE}/anaconda3-2024.02
 # -------------------------------------
 
-if [ ! -d ${anaconda_dir} ]; then
-  # Create installation directory
-  mkdir -p ${anaconda_dir} && rm -rf ${anaconda_dir}
-  # Install conda
-  bash /collab/usr/gapps/python/${SYS_TYPE}/conda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -b -f -p ${anaconda_dir}
-fi
+# Inputs for virtualenv (tuolumne)
+venv_file=requirements.txt
+venv_name=ddnmrom_venv
+virtualenv_dir=/p/lustre5/${USER}/${venv_name}/
 
-source ${anaconda_dir}/bin/activate
-conda env create -f ${env_file}
+# check for machine type: toss_4_x86_64_ib = dane
+#                         toss_4_x86_64_ib_cray = tuolumne
+machine="${SYS_TYPE:-toss_4_x86_64_ib}"
+
+if [[ "${machine}" == "toss_4_x86_64_ib" ]] ;
+then
+    # Dane
+    if [ ! -d ${anaconda_dir} ]; then
+	# Create installation directory
+	mkdir -p ${anaconda_dir} && rm -rf ${anaconda_dir}
+	# Install conda
+	bash /collab/usr/gapps/python/${SYS_TYPE}/conda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -b -f -p ${anaconda_dir}
+    fi
+
+    source ${anaconda_dir}/bin/activate
+    conda env create -f ${env_file}
+else
+    # Tuolumne
+    if [ ! -d ${virtualenv_dir} ]; then
+	# Create virtualenv
+	module load python/3.11.5 rocm/6.3.1
+
+	python3 -m venv ${virtualenv_dir}
+
+	# store directory name for later
+	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+	echo $SCRIPT_DIR
+	echo ${virtualenv_dir} >> ${SCRIPT_DIR}/venv_path.txt
+
+	source ${virtualenv_dir}/bin/activate
+
+	pip install -r ${SCRIPT_DIR}/${venv_file}
+    fi
+
+    source ${virtualenv_dir}/bin/activate
+fi
--- examples/unsteady/commands/generate_data.sh
+++ examples/unsteady/commands/generate_data.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -i
+#flux: -N 1
+#flux: -q pbatch
+#flux: -t 60
+#flux: --exclusive
+#flux: --setattr=thp=always
+#flux: --error=gen_data_err.txt
+#flux: --job-name=gen_data
+#flux: --output=gen_data_out.txt
 
 ### Slurm syntax
 ### ---------------
@@ -14,6 +22,24 @@
 ### ---------------
 ### Loading conda environment thanks to interactive shell
 ### > See: 'dd-nm-rom/conda/README.md' file
-load_conda_env_toss
+machine="${SYS_TYPE:-toss_4_x86_64_ib}"
+
+if [[ "${machine}" == "toss_4_x86_64_ib" ]] ;
+then
+    # Dane
+    load_conda_env_toss
+else
+    # Tuolumne
+    #source ddnmrom_env/bin/activate
+    # todo; assumes this is launched from the commands/ folder
+    venv_dir=$(cat ./../../../conda/llnl_toss/venv_path.txt)
+    echo $venv_dir
+
+    source $venv_dir/bin/activate
+
+    export MPICH_GPU_SUPPORT_ENABLED=1
+    export HSA_XNACK=1
+fi
+
 ### Launch program
 python -u ./../scripts/generate_data.py --inpfile ./../inputs/generate_data.json
