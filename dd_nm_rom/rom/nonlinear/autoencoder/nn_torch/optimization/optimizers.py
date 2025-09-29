d1432eb696ffa86ee20889d5b5c8afdb5b948b54
Cole Kendrick <kendrick6@llnl.gov>
2025-09-29 11:48:59 -0700

    Comment out SOAP optimizer for now to fix runtime error

--- dd_nm_rom/rom/nonlinear/autoencoder/nn_torch/optimization/optimizers.py
+++ dd_nm_rom/rom/nonlinear/autoencoder/nn_torch/optimization/optimizers.py
@@ -1,8 +1,10 @@
 import copy
 import torch
-from .soap import SOAP
+#from .soap import SOAP
 
-_OPTIM_IDS = ("sgd", "rmsprop", "adam", "adamw", "soap")
+#_OPTIM_IDS = ("sgd", "rmsprop", "adam", "adamw", "soap")
+_OPTIM_IDS = ("sgd", "rmsprop", "adam", "adamw")
+
 
 def get(
   params,
@@ -21,7 +23,7 @@ def get(
       "rmsprop": torch.optim.RMSprop,
       "adam":    torch.optim.Adam,
       "adamw":   torch.optim.AdamW,
-      "soap":    SOAP
+      #"soap":    SOAP
     }[optimizer.lower()](params, lr=lr, weight_decay=weight_decay)
   else:
     raise ValueError(
