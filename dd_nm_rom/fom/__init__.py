958b332a5edf7f1f6a3b7b625150011f5f1deacf
Siu Wun Cheung <cheung26@llnl.gov>
2025-10-22 07:04:26 -0700

    Add elasticity

--- dd_nm_rom/fom/__init__.py
+++ dd_nm_rom/fom/__init__.py
@@ -2,12 +2,14 @@ __all__ = [
   "Burgers2D",
   "DDBurgers2D",
   "Poisson2D",
-  "DDPoisson2D"
+  "DDPoisson2D",
+  "Elasticity2D",
+  "DDElasticity2D"
 ]
 
-from .domain_dec import DDBurgers2D, DDPoisson2D
-from .monolithic import Burgers2D, Poisson2D
+from .domain_dec import DDBurgers2D, DDPoisson2D, DDElasticity2D
+from .monolithic import Burgers2D, Poisson2D, Elasticity2D
 
 # Data types
 from typing import Union
-FOM_TYPES = Union[Burgers2D, DDBurgers2D, Poisson2D, DDPoisson2D]
+FOM_TYPES = Union[Burgers2D, DDBurgers2D, Poisson2D, DDPoisson2D, Elasticity2D, DDElasticity2D]
